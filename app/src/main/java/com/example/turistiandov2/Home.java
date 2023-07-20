1592071c82e4a341f0345f818b61b615ffbc0a9e
jose17s <130622077+jose17s@users.noreply.github.com>
2023-07-19 20:42:35 -0500

    finalizamos lo que se necesita en moviles 1

--- app/src/main/java/com/example/turistiandov2/Home.java
+++ app/src/main/java/com/example/turistiandov2/Home.java
@@ -3,6 +3,7 @@ package com.example.turistiandov2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 public class Home extends AppCompatActivity {
 
@@ -71,18 +74,40 @@ public class Home extends AppCompatActivity {
         return true;
     }
 
+    public void cambiaIdioma(String idioma){
+        //configurar elk lenguaje del telefono
+
+        Locale lenguaje = new Locale(idioma);
+        Locale.setDefault(lenguaje);
+
+        //configuramos globalmente el telefono
+
+        Configuration configuraciontelefono = getResources().getConfiguration();
+        configuraciontelefono.locale=lenguaje;
+
+        //ejevutamos la configuracion
+        getBaseContext().getResources().updateConfiguration(configuraciontelefono,getBaseContext().getResources().getDisplayMetrics());
+
+    }
+
 
     public  boolean onOptionsItemSelected(MenuItem item){
         int itemSeleccionado = item.getItemId();
         switch (itemSeleccionado){
             case (R.id.opcion1):
-
+                 this.cambiaIdioma("en");
+                 Intent intentIngles = new Intent(Home.this,Home.class);
+                 startActivity(intentIngles);
                 break;
             case (R.id.opcion2):
-                Toast.makeText(this, "quiero que este en español", Toast.LENGTH_SHORT).show();
+                this.cambiaIdioma("it");
+                Intent intentItaliano = new Intent(Home.this,Home.class);
+                startActivity(intentItaliano);
                 break;
             case (R.id.opcion3):
-                Toast.makeText(this, "quiero que este en italiano", Toast.LENGTH_SHORT).show();
+                this.cambiaIdioma("es");
+                Intent intentespanol = new Intent(Home.this,Home.class);
+                startActivity(intentespanol);
                 break;
             case (R.id.opcion4):
                 Intent intent = new Intent(Home.this,acerca.class);
